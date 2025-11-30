18373996c205bee86d52f572eef4bd25cd19ff3b
pintogrosso <tiagol2006@gmail.com>
2025-11-30 18:23:40 +0000

    salas e character
    uiu

--- src/main/java/com/mycompany/oop2/Character.java
+++ src/main/java/com/mycompany/oop2/Character.java
@@ -9,5 +9,23 @@ package com.mycompany.oop2;
  * @author ajone
  */
 public class Character {
+    String name;
+    Sala salaN;
+    public Character(String name, Sala salaN){
+        this.name = name;
+        this.salaN = salaN;
+    }
     
+    public String getName(){
+        return name;
+    }
+    
+    public Sala getSalaN(){
+        return salaN;
+    }
+    
+    public Sala setSalaN(Sala newSala){
+        return this.salaN = newSala;
+    }
+            
 }
--- src/main/java/com/mycompany/oop2/Sala.java
+++ src/main/java/com/mycompany/oop2/Sala.java
@@ -9,5 +9,64 @@ package com.mycompany.oop2;
  * @author ajone
  */
 public class Sala {
+    String name, description;
+    Sala directionN, directionS, directionE, directionW;
+    public Sala(String name, String description, Sala directionN, Sala directionS, Sala directionE, Sala directionW){
+        this.name = name;
+        this.description = description;
+        this.directionN = directionN;
+        this.directionS = directionS;
+        this.directionE = directionE;
+        this.directionW = directionW;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public String getDescriprion(){
+        return description;
+    }
+    
+    public String setDescription(String X){
+        return this.description = X;
+    }
+    
+    public Sala getDirectionN(){
+        return directionN;
+    }
+    
+    public Sala setDirectionN(Sala N){
+        return this.directionN = N;
+    }
+    
+    public Sala getDirectionS(){
+        return directionS;
+    }
+    
+    public Sala setDirectionS(Sala S){
+        return this.directionS = S;
+    }
+    
+    public Sala getDirectionW(){
+        return directionW;
+    }
+    
+    public Sala setDirectionW(Sala W){
+        return this.directionW = W;
+    }
+    
+    public Sala getDirectionE(){
+        return directionE;
+    }
+    
+    public Sala setDirectionE(Sala E){
+        return this.directionE = E;
+    }
+    
+    @Override
+    public String toString(){
+        return "Sala:" + name +"\n Descricao: " + description +"\n Direcoes: " + directionN +"\n" + directionW + "\n" + directionE +"\n" + directionS;
+    }
     
 }
