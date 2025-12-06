057a6bc35307ea0a7ab88cfbdfe602ec84cd117d
MineJAG <ajonecas@gmail.com>
2025-12-06 20:37:26 +0000

    E

--- src/main/java/Dialogue/DialogueLoader.java
+++ src/main/java/Dialogue/DialogueLoader.java
@@ -1,19 +1,27 @@
 package Dialogue;
 
+import java.io.File;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+import java.io.*;
 /**
  *
  * @author ajone
  */
 public class DialogueLoader {
 
-    public void loadText(String filePath) {
-        
+    public void loadText(String fileName) throws IOException {
+        FileReader fR = new FileReader(fileName);
+        BufferedReader bR = new BufferedReader(fR);
+        String line;
+        while ((line = bR.readLine()) != null) {
+            if(line.charAt(0) == '#'){
+                continue;
+            }
+            System.out.println(line);
+        }
     }
-
-    
 }
