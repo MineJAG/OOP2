bad01e456fb98e565eac8547dbd623f56f6c4a7a
MineJAG <ajonecas@gmail.com>
2025-12-08 01:12:33 +0000

    yoooooo

--- src/main/java/Dialogue/DialogueDisplay.java
+++ src/main/java/Dialogue/DialogueDisplay.java
@@ -22,13 +22,14 @@ public class DialogueDisplay {
         for(int i = 0; i < options.size(); i++){
             switch (options.get(i).getNextLineId().charAt(0)) {
                 case '!':
-                    System.out.println((i) + ". " + options.get(i).getOption());
+                    if(player.getInventory().containsItem(options.get(i).getNextLineId().substring(1)))
+                        System.out.println((i+1) + ". " + options.get(i).getOption());
                     break;
                 case '?':
-                    System.out.println((i) + ". " + options.get(i).getOption());
+                    System.out.println((i+1) + ". " + options.get(i).getOption());
                     break;
                 default:
-                    System.out.println((i) + ". " + options.get(i).getOption());
+                    System.out.println((i+1) + ". " + options.get(i).getOption());
             }
         }
     }
